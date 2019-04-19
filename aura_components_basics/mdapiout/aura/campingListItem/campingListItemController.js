a1159bd83056ca1db51a99c0829ab963c3268024
johnconnelly75 <johnconnelly75@gmail.com>
2019-04-19 19:41:31 -0400

    phew - that was a long session. and i haven't done the exercise yet...

--- aura_components_basics/mdapiout/aura/campingListItem/campingListItemController.js
+++ aura_components_basics/mdapiout/aura/campingListItem/campingListItemController.js
@@ -1,5 +1,8 @@
 ({
-    myAction : function(component, event, helper) {
-
+    packItem : function(component, event, helper) {
+        var item = component.get("v.item",true);
+        item.Packed__c = true
+        component.set("v.item", item);
+        event.getSource().set("v.disabled",true);
     }
 })
